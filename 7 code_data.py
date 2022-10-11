381cd14666d6675d286133a17d66599e2d384e9f
ParkMinjeong <minwjd1028@g.skku.edu>
2022-10-11 11:14:46 +0900

    221011_change
    1. code_data.py에서 Lcto 관련 내용 삭제

--- 7 code_data.py
+++ 7 code_data.py
@@ -13,22 +13,20 @@ Pcd1_name = {"03": "석기", "05": "청동기", "07": "철기", #3
 
 heritage_df = csv_to_df("heritage.csv")
 Ctcd_code_df = pd.DataFrame()
-Lcto_code_df = pd.DataFrame()
+# Lcto_code_df = pd.DataFrame()
 Pcd1_code_df = pd.DataFrame()
 
 for Ctcd in sorted(heritage_df["ccbaCtcd"].unique()):
     tmp = pd.DataFrame([[Ctcd, Ctcd_name[Ctcd]]], columns=["ccbaCtcd", "ccbaCtcdNm"])
     Ctcd_code_df = pd.concat([Ctcd_code_df, tmp], ignore_index=True)
 
-    if Ctcd == "45":
-        continue
-
-    Lcto_df = csv_to_df(f"ccbaLcto_{Ctcd}.csv", ccbaLcto_path)
-
-    for Lcto in sorted(heritage_df[heritage_df["ccbaCtcd"]==Ctcd]["ccbaLcto"].unique()):
-        name = Lcto_df[Lcto_df["ccbaLcto_item"]==Lcto]["ccbaLctoNm_item"].iloc[0]
-        tmp = pd.DataFrame([[Ctcd, Lcto, name]], columns=["ccbaCtcd", "ccbaLcto", "ccbaLctoNm"])
-        Lcto_code_df = pd.concat([Lcto_code_df, tmp], ignore_index=True)
+    # if Ctcd == "45":
+    #     continue
+    # Lcto_df = csv_to_df(f"ccbaLcto_{Ctcd}.csv", ccbaLcto_path)
+    # for Lcto in sorted(heritage_df[heritage_df["ccbaCtcd"]==Ctcd]["ccbaLcto"].unique()):
+    #     name = Lcto_df[Lcto_df["ccbaLcto_item"]==Lcto]["ccbaLctoNm_item"].iloc[0]
+    #     tmp = pd.DataFrame([[Ctcd, Lcto, name]], columns=["ccbaCtcd", "ccbaLcto", "ccbaLctoNm"])
+    #     Lcto_code_df = pd.concat([Lcto_code_df, tmp], ignore_index=True)
 
 
 for Pcd1 in sorted(heritage_df["ccbaPcd1"].unique()):
@@ -37,4 +35,4 @@ for Pcd1 in sorted(heritage_df["ccbaPcd1"].unique()):
 
 save_df(Ctcd_code_df, "Ctcd_code.csv")
 save_df(Pcd1_code_df, "Pcd1_code.csv")
-Lcto_code_df.to_csv("./data/Lcto_code.csv", encoding="utf-8-sig", index=True, index_label="id")
+# Lcto_code_df.to_csv("./data/Lcto_code.csv", encoding="utf-8-sig", index=True, index_label="id")
